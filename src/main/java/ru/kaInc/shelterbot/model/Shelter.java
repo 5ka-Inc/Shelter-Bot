e7f0964815b94af3a944fc9fe29c5b1c72313132
HellHorseman <duke.sheffer@yandex.ru>
2023-10-18 22:24:03 +0400

    Add class Shelter. Entity, getter, setter

---
+++ src/main/java/ru/kaInc/shelterbot/model/Shelter.java
@@ -0,0 +1,55 @@
+package ru.kaInc.shelterbot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "shelters")
+public class Shelter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Shelter() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelter shelter = (Shelter) o;
+        return Objects.equals(id, shelter.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
