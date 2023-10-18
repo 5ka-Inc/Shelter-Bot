da00aa4bc1faf041673cfc0c7c993e7acf27d584
fifimova <fi_fimova@mail.ru>
2023-10-18 16:52:44 +0300

    #1 -поправила табуляцию

--- src/main/resources/liquibase/scripts/V_1_1__add_fk.sql
+++ src/main/resources/liquibase/scripts/V_1_1__add_fk.sql
@@ -8,4 +8,4 @@ ALTER TABLE users
     ADD CONSTRAINT users_volunteer_fk FOREIGN KEY (volunteer_id) REFERENCES volunteers (id) ON DELETE CASCADE;
 
 ALTER TABLE volunteers
-ADD CONSTRAINT volunteers_shelter_fk FOREIGN KEY (shelter_id) REFERENCES shelters (id) ON DELETE CASCADE;
+    ADD CONSTRAINT volunteers_shelter_fk FOREIGN KEY (shelter_id) REFERENCES shelters (id) ON DELETE CASCADE;
