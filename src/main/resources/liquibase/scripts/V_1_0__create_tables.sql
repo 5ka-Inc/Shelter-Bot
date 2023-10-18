5e3a81e63d3a1ec5fa0a3180dbbd1f029476b7b0
fifimova <fi_fimova@mail.ru>
2023-10-18 17:59:57 +0300

    -поправила табуляцию

--- src/main/resources/liquibase/scripts/V_1_0__create_tables.sql
+++ src/main/resources/liquibase/scripts/V_1_0__create_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE users
     phone        TEXT,
     shelter_id   UUID,
     volunteer_id UUID,
-    is_adopter    BOOLEAN,
+    is_adopter   BOOLEAN,
 
     CONSTRAINT user_pk PRIMARY KEY (id)
 );
